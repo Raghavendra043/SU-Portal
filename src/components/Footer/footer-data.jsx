d0878cfbdabf913df4c314e61bb2d4572930e404
poddarrishi02 <poddarrishi02@gmail.com>
2021-09-07 04:57:00 +0530

    Feg Header added, main Footer added

---
+++ src/components/Footer/footer-data.jsx
@@ -0,0 +1,69 @@
+const footerData = [
+    {
+        heading:"Quick Links",
+        col1:
+            [
+                {
+                    id: 1,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 2,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 3,
+                    content: "Lorem ipsum dolor"
+                }
+            ],
+        col2:
+            [
+                {
+                    id: 1,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 2,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 3,
+                    content: "Lorem ipsum dolor"
+                }
+            ]
+    },
+    {
+        heading:"Contact Us",
+        col1:
+            [
+                {
+                    id: 1,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 2,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 3,
+                    content: "Lorem ipsum dolor"
+                }
+            ],
+        col2:
+            [
+                {
+                    id: 1,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 2,
+                    content: "Lorem ipsum dolor"
+                },
+                {
+                    id: 3,
+                    content: "Lorem ipsum dolor"
+                }
+            ]
+    }
+]
+export default footerData
