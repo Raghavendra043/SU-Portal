d9c273fdfca35fe6014e5a3fef4b322c70225f17
ishan <sangalishan1234@gmail.com>
2021-09-06 23:03:38 +0530

    data variable added

--- src/pages/clubs/clubname.js
+++ src/pages/clubs/clubname.js
@@ -1,7 +1,8 @@
 import React from "react";
 import image from "./images/clubimage.png";
 import "./clubname.css";
-import clublogo from "./images/Dota logo-01.png";
+// import clublogo from "./images/Dota logo-01.png";
+import { dota } from "./data";
 export default function clubname() {
   return (
     <>
@@ -9,19 +10,12 @@ export default function clubname() {
         <img src={image} alt="" srcset="" className="image" />
       </div>
       <div>
-        <img src={clublogo} alt="" srcset="" className="clublogo" />
+        <img src={dota.Name.logo} alt="" srcset="" className="clublogo" />
       </div>
-      <div className="clubname">Department of Technical Art</div>
+      <div className="clubname">{dota.Name.name}</div>
       <div className="club_aboutus">
-        <h5>About us</h5>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-          ullam, sunt ea quibusdam quisquam eveniet, reprehenderit laboriosam
-          corporis in ipsam tenetur aut saepe iste, illum rerum voluptates
-          facere temporibus nihil doloribus mollitia dicta nulla quae commodi.
-          Blanditiis recusandae, non atque autem temporibus beatae, iure
-          dignissimos suscipit, itaque quas architecto nemo?
-        </p>
+        <h5>{dota.Name.aboutHeading}</h5>
+        <p>{dota.Name.aboutPara}</p>
       </div>
     </>
   );
---
+++ src/pages/clubs/data.js
@@ -0,0 +1,45 @@
+import dotalogo from "./images/Dota logo-01.png";
+export const dota = {
+  Name: {
+    logo: `${dotalogo}`,
+    name: "Department of Technical Art",
+    aboutHeading: "About us",
+    aboutPara:
+      "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impeditullam, sunt ea quibusdam quisquam eveniet, reprehenderit laboriosamcorporis in ipsam tenetur aut saepe iste, illum rerum voluptatesfacere temporibus nihil doloribus mollitia dicta nulla quae commodi.Blanditiis recusandae, non atque autem temporibus beatae, iuredignissimos suscipit, itaque quas architecto nemo?",
+  },
+  events: [
+    {
+      id: "first",
+      date: 22,
+      month: "August",
+      heading: "Campus Event Headline",
+      para: "     Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptates, ipsa earum? Magni cumque quis ex minus, eveniet ametprovident quas ratione cum aperiam, dolor velit impedit facerequisquam? Quos, error.",
+
+      color: "rgb(218, 37, 29)",
+    },
+    {
+      id: "second",
+      date: 23,
+      month: "August",
+      heading: "Campus Event Headline",
+      para: "     Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptates, ipsa earum? Magni cumque quis ex minus, eveniet ametprovident quas ratione cum aperiam, dolor velit impedit facerequisquam? Quos, error.",
+      color: "#45b43b",
+    },
+    {
+      id: "third",
+      date: 24,
+      month: "August",
+      heading: "Campus Event Headline",
+      color: " #2d7dd2",
+      para: "     Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptates, ipsa earum? Magni cumque quis ex minus, eveniet ametprovident quas ratione cum aperiam, dolor velit impedit facerequisquam? Quos, error.",
+    },
+    {
+      id: "fourth",
+      date: 25,
+      month: "August",
+      heading: "Campus Event Headline",
+      color: "#ffc300",
+      para: "     Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptates, ipsa earum? Magni cumque quis ex minus, eveniet ametprovident quas ratione cum aperiam, dolor velit impedit facerequisquam? Quos, error.",
+    },
+  ],
+};
