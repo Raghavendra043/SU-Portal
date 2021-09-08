bccd2cab406e6219ef1c1ce86ae09d8079b91182
Srieon <speedshashwat@gmail.com>
2021-09-09 02:50:40 +0530

    Footer and Feg Added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Change from "./pages/Change/Change";
 import Places from "./pages/Places/Places";
-import Studentlife from "./pages/Studentlife/Studentlife";
+import FegComponent from "./pages/Feg/FegComponent";
 import HelpAndAdvice from "./pages/HelpAndAdvice/HelpAndAdvice";
 import Navbar from './components/Navbar new/Navbar';
 import Clubs from "./pages/clubs/clubs";
@@ -33,7 +33,7 @@ const App = () => {
             <Route path='/about' exact component={About} />
             <Route path='/change' exact component={Change} />
             <Route path='/places' exact component={Places} />
-            <Route path='/studentlife' exact component={Studentlife} />
+            <Route path='/studentlife' exact component={FegComponent} />
             <Route path='/helpandadvice' exact component={HelpAndAdvice} />
             <Route path='/contact' exact component={Contact} />
             <Redirect to="/" />
