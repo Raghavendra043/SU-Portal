9a0b99a00e2dc999a5291941090ea422db8f9cb3
AnishGuruvelli <guruvellianish@gmail.com>
2021-09-08 19:22:49 +0530

    Merge branch 'main' of https://github.com/Raghavendra043/SU-Portal

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-pascal-case */
 import React from "react";
 import path1 from "../../assets/home/hero/path1.svg";
 import heroImg from "../../assets/home/hero/heroImg.svg";
 import Campus_Events from "../../components/Home/Campus_Events";
+import WelcomeOnBoard from "../../components/Home/WelcomeOnBoard";
 import SU_Initiatives from "../../components/Home/SU_Initiatives";
 import "./Home.css";
 
@@ -23,23 +26,42 @@ function Home() {
         </div>
       </div>
 
-        {/* Campus Events */}
-      
+      {/* Campus News */}
+
+      <h5 style={{ marginTop: "8vh", marginBottom: "-1vh", textAlign: "center" }}><b>Campus News :</b> Lorem ipsum dolor sit | Lorem ipsum dolor sit | Lorem ipsum dolor sit | Lorem ipsum dolor sit</h5>
+
+      {/* Welcome On Board */}
+
+      <div>
+          <WelcomeOnBoard />
+      </div>
+
+      {/* Campus Events */}
+
       <div>
         <p className="header">Campus Events</p>
         <div className="container">
-          <Campus_Events title="Campus Event Headline" width="25vw" date="21" month="Dec"/>
-          <Campus_Events title="Campus Event Headline" width="30vw" date="18" month="Sept"/>
-          <Campus_Events title="Campus Event Headline" width="30vw" date="25" month="August"/>
-          <Campus_Events title="Campus Event Headline" width="25vw" date="01" month="Sept"/>
+          <Campus_Events title="Campus Event Headline" width="25vw" date="21" month="Dec" />
+          <Campus_Events title="Campus Event Headline" width="30vw" date="18" month="Sept" />
+          <Campus_Events title="Campus Event Headline" width="30vw" date="25" month="August" />
+          <Campus_Events title="Campus Event Headline" width="25vw" date="01" month="Sept" />
           <a href="#" className="more">
             <i class="fa fa-lg fa-chevron-right" aria-hidden="true"></i>
           </a>
         </div>
       </div>
 
-        {/* SU Initiaves */}
-      
+      {/* Welcome Freshers */}
+
+      <div>
+        <p className="header">Welcome Freshers</p>
+        <div className="container">
+
+        </div>
+      </div>
+
+      {/* SU Initiaves */}
+
       <div>
         <p className="header">SU Initiatives</p>
         <div className="container">
@@ -52,7 +74,15 @@ function Home() {
         </div>
       </div>
 
-        {/* Built By */}
+      {/* Know Your SUC */}
+
+      <div>
+        <p className="header">Know Your SUC</p>
+        <div className="container">
+        </div>
+      </div>
+
+      {/* Built By */}
 
       <div>
         <p className="header">Built By</p>
