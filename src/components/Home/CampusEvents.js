9552a4162b117e6de9513a8b793fa18dcf5ee7ce
AnishGuruvelli <guruvellianish@gmail.com>
2021-09-09 10:10:03 +0530

    home page debugging

--- src/components/Home/CampusEvents.js
+++ src/components/Home/CampusEvents.js
@@ -1,33 +1,33 @@
 import React from 'react'
 
 export default function Campus_Events(props) {
-    return (
-        <div className="card" style= {{width: props.width}}>
-            <p
-                style={{
-                  lineHeight: "30px",
-                  margin: "10px 30%",
-                  fontWeight: "bold",
-                  zIndex: "100",
-                  width: "65%"
-                }}
-              >
-                {props.title}
-              </p>
-              <p style={{ fontSize: "0.7rem" , margin:"0 30%", maxHeight: "50%", overflow: "hidden", width: "65%" }}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.ullam
-                repellendus nobis rem nemo. Natus iste voluptas ab ratione
-                cupiditate.Natus iste voluptas ab ratione
-                cupiditate.Natus iste voluptas ab ratione
-                cupiditate.
-              </p>
-            <div className="overlay">
-              <div className="slide">
-                  <button>Notify me</button>
-              </div>
-              <h2>{props.date}</h2>
-              <p className="Date" style={{marginTop: "-15px", marginLeft: "3px"}}>{props.month}</p>
-            </div>
-          </div>
-    )
+  return (
+    <div className="card" style={{ width: props.width }}>
+      <p
+        style={{
+          lineHeight: "30px",
+          margin: "10px 30%",
+          fontWeight: "bold",
+          zIndex: "100",
+          width: "65%"
+        }}
+      >
+        {props.title}
+      </p>
+      <p style={{ fontSize: "0.7rem", margin: "0 30%", maxHeight: "50%", overflow: "hidden", width: "65%" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.ullam
+        repellendus nobis rem nemo. Natus iste voluptas ab ratione
+        cupiditate.Natus iste voluptas ab ratione
+        cupiditate.Natus iste voluptas ab ratione
+        cupiditate.
+      </p>
+      <div className="overlay">
+        <div className="slide">
+          <button>Notify me</button>
+        </div>
+        <h2>{props.date}</h2>
+        <p className="Date" style={{ marginTop: "-15px", marginLeft: "3px" }}>{props.month}</p>
+      </div>
+    </div>
+  )
 }
