e402974aea5cda1c7d59d59d42ff2323e744f51d
AnishGuruvelli <guruvellianish@gmail.com>
2021-09-09 22:05:20 +0530

    student life page added

---
+++ src/pages/Studentlife/StudentlifeData.js
@@ -0,0 +1,34 @@
+import backimage from "./bitscampusdark.jpg";
+export const studentlife = {
+  leftheading: "Student Life",
+  rightheading: [
+    {
+      heading: "Fests",
+    },
+    {
+      heading: "Departments",
+    },
+    {
+      heading: "Cultural Senate",
+    },
+    {
+      heading: "Sports Senate",
+    },
+    {
+      heading: "Tech Senate",
+    },
+    {
+      heading: "Societies",
+    },
+    {
+      heading: "Volunteering",
+    },
+    {
+      heading: "Others (tedx, embryo, raf etc.)",
+    },
+  ],
+  leftpara:
+    "   Lorem ipsum dolor, sit amet consectetur adipisicing elit.Voluptates quibusdam, nulla voluptate officiis eumreiciendis quia quo aliquid consequatur quidem ea dolorumtempore! Esse cum magni nesciunt. Culpa voluptatum officiarepellat assumenda in harum officiis veritatis et nobis istemagnam, placeat blanditiis rem perferendis molestiaemollitia sit accusantium optio enim. Dolorem nisi aut nonobcaecati, voluptas modi magnam autem at amet animi.Repudiandae, magni neque unde accusamus ipsa animi ipsum?",
+  bottomheading: "Student",
+  backimage: `${backimage}`,
+};
