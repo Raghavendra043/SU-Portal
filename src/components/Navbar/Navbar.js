a35a03319b3629849113c1d3546d94c450541269
prabalp <prabalpanda2008@gmail.com>
2021-09-27 23:03:14 +0530

    adjustments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
-import './Navbar.css';
-import { NavLink } from 'react-router-dom';
-import $ from 'jquery';
+import React, { useEffect } from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import $ from "jquery";
 // import logo from './logo.png';
 // import HomeIcon from '@material-ui/icons/Home';
 // import InfoIcon from '@material-ui/icons/Info';
@@ -11,67 +11,70 @@ import $ from 'jquery';
 // import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
 
 const Navbar = () => {
-
   function animation() {
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
+    var tabsNewAnim = $("#navbarSupportedContent");
+    var activeItemNewAnim = tabsNewAnim.find(".active");
     var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
     var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
     var itemPosNewAnimTop = activeItemNewAnim.position();
     var itemPosNewAnimLeft = activeItemNewAnim.position();
     $(".hori-selector").css({
-      "top": itemPosNewAnimTop.top + "px",
-      "left": itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
+      top: itemPosNewAnimTop.top + "px",
+      left: itemPosNewAnimLeft.left + "px",
+      height: activeWidthNewAnimHeight + "px",
+      width: activeWidthNewAnimWidth + "px",
     });
     $("#navbarSupportedContent").on("click", "li", function (e) {
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
+      $("#navbarSupportedContent ul li").removeClass("active");
+      $(this).addClass("active");
       var activeWidthNewAnimHeight = $(this).innerHeight();
       var activeWidthNewAnimWidth = $(this).innerWidth();
       var itemPosNewAnimTop = $(this).position();
       var itemPosNewAnimLeft = $(this).position();
       $(".hori-selector").css({
-        "top": itemPosNewAnimTop.top + "px",
-        "left": itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
+        top: itemPosNewAnimTop.top + "px",
+        left: itemPosNewAnimLeft.left + "px",
+        height: activeWidthNewAnimHeight + "px",
+        width: activeWidthNewAnimWidth + "px",
       });
     });
   }
 
   useEffect(() => {
-
     animation();
-    $(window).on('resize', function () {
-      setTimeout(function () { animation(); }, 500);
+    $(window).on("resize", function () {
+      setTimeout(function () {
+        animation();
+      }, 500);
     });
-
   }, []);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-mainbg">
-
       <NavLink className="navbar-brand navbar-logo" to="/" exact>
         {/* <img src={logo} alt="BPHC SUC LOGO" /> */}
         BPHC SUC LOGO
       </NavLink>
 
-
       <button
         className="navbar-toggler"
         onClick={function () {
-          setTimeout(function () { animation(); });
+          setTimeout(function () {
+            animation();
+          });
         }}
-        type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <i className="fas fa-bars text-white"></i>
       </button>
 
-      <div
-        className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
-
           <div className="hori-selector">
             <div className="left"></div>
             <div className="right"></div>
@@ -122,7 +125,7 @@ const Navbar = () => {
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
