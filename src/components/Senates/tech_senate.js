64a2fa62c2dd5a883976c6bafba962f1bb93454d
Raghavendra Kokkaraipalli <f20190120@hyderabad.bits-pilani.ac.in>
2021-10-31 15:35:05 +0530

    changes

--- src/components/Senates/tech_senate.js
+++ src/components/Senates/tech_senate.js
@@ -8,13 +8,13 @@ import techBg from '../../assets/images/Senates/deptsbg.svg'
 function tech_senate() {
     return (
         <>
-            <Page1
+            {/* <Page1
                 bg={techBg}
                 heading="Technical Senate"
                 content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing"
                 leftimg={leftImage}
                 rightimg={rightImage}
-            />
+            /> */}
             <Assocs />
             <Clubs />
         </>
