8828dfa5688533cf0e47b33e052caf86eb37ca59
ishan <sangalishan1234@gmail.com>
2022-01-09 20:37:13 +0530

    food outlet updated

--- src/components/FoodOutlets/Outlet.js
+++ src/components/FoodOutlets/Outlet.js
@@ -1,20 +1,31 @@
 import React from "react";
+import "./outlet.css";
 
 export default function Outlet(props) {
-  const name = props.Name;
   return (
-    <div className="outlet">
-      <div className="part1">
-        <h1>{name}</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-          dolore voluptatem aut praesentium odio unde fuga totam laudantium
-          nobis sunt, dolorem officia! Voluptatum, necessitatibus ipsum!
-        </p>
-      </div>
-      <div className="part2">
-          <img src="" alt="This is a picture" />
+    <div>
+      <div
+        className="outletouterbox"
+        style={{ backgroundColor: `${props.fooddata.backcolor}` }}
+      >
+        <div
+          className="outletleftbox"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            minHeight: "200px",
+          }}
+        >
+          <h1>{props.fooddata.name}</h1>
+          <p style={{ width: "90%", fontSize: "10px" }}>
+            {props.fooddata.content}
+          </p>
         </div>
+        <div className="outletrightbox">
+          {/* <img src={props.fooddata.svg} alt="" className="outletimg" /> */}
+        </div>
+      </div>
     </div>
   );
 }
