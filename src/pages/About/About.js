47c4deab888afe5aa4ecd5e4e68205cbb495e2ae
Amarjeet Mohanty <amarmohanty06112003@gmail.com>
2023-08-03 00:23:00 +0530

    set up the toggling in about us

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,88 +1,86 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import aboutcss from "./AboutContent.module.css";
 import { Link } from "react-router-dom";
 import { about } from "./data";
 
 export default function About() {
-  const [selectedItem, setSelectedItem] = useState(about.rightheading[0]);
+	const [selectedItem, setSelectedItem] = useState(about.rightheading[0]);
 
-  const handleItemClick = (item)=>{
-    setSelectedItem(item);
-  }
-  const back = () => {
-    window.history.go(-1);
-  };
+	const handleItemClick = (item) => {
+		setSelectedItem(item);
+	};
+	const back = () => {
+		window.history.go(-1);
+	};
 
-  return (
-    <div className={aboutcss.aboutfont}>
-      <div
-        className={aboutcss.aboutsection}
-        style={{
-          display: `flex`,
-          flexDirection: `column`,
-          margin: `auto`,
-          background: `linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url(${about.backimage}) no-repeat center center/cover`,
-          backgroundSize: "cover",
-          position: `absolute`,
-          top: `0px`,
-          zIndex: -2,
-          width: `100%`,
-          minHeight: `100vh`,
-          color: `white`,
-        }}
-      >
-        <div className={aboutcss.aboutbackimage}></div>
-        <div className={aboutcss.aboutmain}>
-          <div className={aboutcss.aboutback}>
-            <i
-              className="fa fa-chevron-left"
-              style={{ fontSize: "2em" }}
-              aria-hidden="true"
-              onClick={back}
-            ></i>
-            <h6
-              style={{ marginBottom: "0px", marginLeft: "10px" }}
-              onClick={back}
-            >
-              Back
-            </h6>
-          </div>
-          <div className={aboutcss.aboutcontent}>
-          <div className={aboutcss.aboutleft}>
-  <Fade direction="left" delay={300} triggerOnce>
-    <div className={aboutcss.aboutleftheading}>
-      <h1>{selectedItem.heading}</h1>
-      <p style={{ marginTop: "1rem" }}>{selectedItem.content}</p>
-    </div>
-  </Fade>
-</div>
+	return (
+		<div className={aboutcss.aboutfont}>
+			<div
+				className={aboutcss.aboutsection}
+				style={{
+					display: `flex`,
+					flexDirection: `column`,
+					margin: `auto`,
+					background: `linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url(${about.backimage}) no-repeat center center/cover`,
+					backgroundSize: "cover",
+					position: `absolute`,
+					top: `0px`,
+					zIndex: -2,
+					width: `100%`,
+					minHeight: `100vh`,
+					color: `white`,
+				}}
+			>
+				<div className={aboutcss.aboutbackimage}></div>
+				<div className={aboutcss.aboutmain}>
+					<div className={aboutcss.aboutback}>
+						<i
+							className="fa fa-chevron-left"
+							style={{ fontSize: "2em" }}
+							aria-hidden="true"
+							onClick={back}
+						></i>
+						<h6
+							style={{ marginBottom: "0px", marginLeft: "10px" }}
+							onClick={back}
+						>
+							Back
+						</h6>
+					</div>
+					<div className={aboutcss.aboutcontent}>
+						<div className={aboutcss.aboutleft}>
+							<Fade direction="left" delay={300} triggerOnce>
+								<div className={aboutcss.aboutleftheading}>
+									<h1>{selectedItem.heading}</h1>
+									<p style={{ marginTop: "1rem" }}>{selectedItem.content}</p>
+								</div>
+							</Fade>
+						</div>
 
-
-<div className={aboutcss.aboutright}>
-  <Fade direction="right" delay={300} triggerOnce>
-    {about.rightheading.map((item) => (
-      <div key={item.id} className={aboutcss.link}>
-        {item.id === "su-team" || item.id === "contact" ? (
-          <Link to={`/${item.id}`} className={aboutcss.customLink}>
-          <h1>{item.title}</h1>
-        </Link>
-        ) : (
-          <h1 onClick={() => handleItemClick(item)}>{item.title}</h1>
-        )}
-      </div>
-    ))}
-  </Fade>
-</div>
-
-
-
-          </div>
-          <div className={aboutcss.aboutbottomheading}>
-            <h1>{about.bottomheading}</h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<div className={aboutcss.aboutright}>
+							<Fade direction="right" delay={300} triggerOnce>
+								{about.rightheading.map((item) => (
+									<div key={item.id} className={aboutcss.link}>
+										{item.id === "su-team" || item.id === "contact" ? (
+											<Link to={`/${item.id}`} className={aboutcss.customLink}>
+												<h1>{item.title}</h1>
+											</Link>
+										) : (
+											<h1 onClick={() => handleItemClick(item)}>
+												{item.title}
+											</h1>
+										)}
+									</div>
+								))}
+							</Fade>
+						</div>
+					</div>
+					<div className={aboutcss.aboutbottomheading}>
+						<h1>{about.bottomheading}</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/About/data.js
+++ src/pages/About/data.js
@@ -1,44 +1,42 @@
 import backimage from "./Union1.jpeg";
 export const about = {
-  rightheading: [
-    {
-      id: "about-us",
-      title: "About Us",
-      heading: "About Us",
-      content: "The students of 'BPHC', as we dearly call it, strive to achieve the highest levels of excellence in the endeavours they take upon themselves. We bring each and every students' voice to the table and represent them in their best interest while providing an amicable solution to all their concerns. We preserve and protect the culture and legends of our University for future generations. We are the torch bearers of our college who take upon the task of ensuring the  vibrant culture of the BITSian community is upheld perpetually. We solemnly resolve to - protect the rights of the student community by all fair means available to us, endeavor to impart a sense of fair-play and security among the students, promote all activities relating to academic awareness, cultural affluence, physical welfare and general welfare; develop the Institute into a centre of technological excellence and to uphold and promote the core values of integrity, righteousness and liberty."
-    },
-    {
-      id: "the-suc",
-      title: "The SUC",
-      heading: "The SUC",
-      content:
-        "The Students' Union Council of BITS Hyderabad, is a body of representatives elected by the student community. It acts as a bridge between the students and the administration and is responsible for the smooth functioning of campus activities. The Council comprises: The President, The General Secretary, Cultural Secretaries and the Sports Secretaries. The 6 elected representatives assisted by their team, work for the all-round development of the campus.",
-    },
-    {
-      id: "our-responsibilities",
-      title: "Our Responsibilities",
-      heading: "Responsibilities",
-      content:"Roles:                 The President: The President is the head of the SUC and all matters concerning the fests and more come under his/her purview.      The General Secretary: The General Secretary is second in line. He/She is responsible for managing all the issues related to the functioning of various bodies on campus.       The Cultural Secretaries: They are the heads of the Cultural Senate and are responsible for upheaving the cultural aspect of the campus.       The Sports Secretaries: They are the heads of the sports senate and are responsible for the overall betterment of the sports culture on campus.      ",
-    },
-    {
-      id: "ur-vision",
-      title: "Our Vision",
-      heading: "Our Vision for the Future",
-      content:"The students of 'BPHC', as we dearly call it, strive to achieve the highest levels of excellence in the endeavors they take upon themselves. We bring each and every students' voice to the table and represent them in their best interest while providing an amicable solution to all their concerns.      We preserve and protect the traditions and legends of our University for future generations.      ",
-    },
-    { id: "su-team",
-      title: "SU Team",
-      heading: "SU Team ",
-    },
-    { id: "batch-reps",
-      title: "Batch Reps",
-      heading: "Batch Reps/Hostel Reps",
-    },
-    { id: "contact",
-      title: "Contact us",
-      heading: "Contact us",
-    },
-  ],
-  bottomheading: "About",
-  backimage: `${backimage}`,
+	rightheading: [
+		{
+			id: "about-us",
+			title: "About Us",
+			heading: "About Us",
+			content:
+				"The students of 'BPHC', as we dearly call it, strive to achieve the highest levels of excellence in the endeavours they take upon themselves. We bring each and every students' voice to the table and represent them in their best interest while providing an amicable solution to all their concerns. We preserve and protect the culture and legends of our University for future generations. We are the torch bearers of our college who take upon the task of ensuring the  vibrant culture of the BITSian community is upheld perpetually. We solemnly resolve to - protect the rights of the student community by all fair means available to us, endeavor to impart a sense of fair-play and security among the students, promote all activities relating to academic awareness, cultural affluence, physical welfare and general welfare; develop the Institute into a centre of technological excellence and to uphold and promote the core values of integrity, righteousness and liberty.",
+		},
+		{
+			id: "the-suc",
+			title: "The SUC",
+			heading: "The SUC",
+			content:
+				"The Students' Union Council of BITS Hyderabad, is a body of representatives elected by the student community. It acts as a bridge between the students and the administration and is responsible for the smooth functioning of campus activities. The Council comprises: The President, The General Secretary, Cultural Secretaries and the Sports Secretaries. The 6 elected representatives assisted by their team, work for the all-round development of the campus.",
+		},
+		{
+			id: "our-responsibilities",
+			title: "Our Responsibilities",
+			heading: "Responsibilities",
+			content:
+				"Roles:                 The President: The President is the head of the SUC and all matters concerning the fests and more come under his/her purview.      The General Secretary: The General Secretary is second in line. He/She is responsible for managing all the issues related to the functioning of various bodies on campus.       The Cultural Secretaries: They are the heads of the Cultural Senate and are responsible for upheaving the cultural aspect of the campus.       The Sports Secretaries: They are the heads of the sports senate and are responsible for the overall betterment of the sports culture on campus.      ",
+		},
+		{
+			id: "ur-vision",
+			title: "Our Vision",
+			heading: "Our Vision for the Future",
+			content:
+				"The students of 'BPHC', as we dearly call it, strive to achieve the highest levels of excellence in the endeavors they take upon themselves. We bring each and every students' voice to the table and represent them in their best interest while providing an amicable solution to all their concerns.      We preserve and protect the traditions and legends of our University for future generations.      ",
+		},
+		{ id: "su-team", title: "SU Team", heading: "SU Team " },
+		{
+			id: "batch-reps",
+			title: "Batch Reps",
+			heading: "Batch Reps/Hostel Reps",
+		},
+		{ id: "contact", title: "Contact us", heading: "Contact us" },
+	],
+	bottomheading: "About",
+	backimage: `${backimage}`,
 };
