e8940a65b0789c16f87dc04fda888ce892b2f120
Amarjeet Mohanty <amarmohanty06112003@gmail.com>
2023-08-06 20:58:55 +0530

    changed package.json

--- src/pages/academic/Academic.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Acadblock from "../../components/academic/Acadblock";
-export default function Academic() {
-  return (
-    <div>
-      <Acadblock />
-    </div>
-  );
-}
