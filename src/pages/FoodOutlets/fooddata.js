d97d169afc82a8fc24f5c9cab392e282f00eb091
Ojus Kadu <gamers316ftw@gmail.com>
2023-08-07 23:18:14 +0530

    adjusted heights of all sections in mobile view and tablet view

--- src/pages/FoodOutlets/fooddata.js
+++ src/pages/FoodOutlets/fooddata.js
@@ -14,6 +14,7 @@ export const fooddata = {
       "A cafe of sorts, C3 is a place where you'd sometimes find music practices and midnight birthday celebrations. C3 opens up in the evening and has a nice atmosphere with the lights as decoration. They serve Indian snacks like pav bhaji and chole bhature, sandwiches as well as Chinese dishes like fried rice, noodles, honey chicken. They also offer different, exciting food options in their special menus which are updated regularly, They keep a stock of brownies, pastries, cakes, and different types of iced teas and mojitos. You can always turn to Cold Lab, which is an extension of C3. for a vast range of sundaes, brownies and other delectable desserts.",
     svg: `${svg1}`,
     backcolor: " #ed8f72",
+    id: "c3",
   },
   second: {
     name: "Wich Please",
@@ -21,6 +22,7 @@ export const fooddata = {
       "As the name suggests, this is your stop shop for a quick bite. Known for its sandwiches, this place offers snacks at an average affordable price of around ₹40. While you are here, don't forget to try their famous pasta sandwich. Want to eat something different yet cheap and light? They do serve the all-loved classic Maggi which is also available in 2-3 variations; their timings being 5 PM-2 AM on a regular day.",
     svg: `${svg2}`,
     backcolor: " #FFD60A",
+    id: "wich",
   },
   third: {
     name: "Fruitful",
@@ -28,6 +30,7 @@ export const fooddata = {
       "A snack shop in the CP, Fruitful has quick munches like fries, burgers, cheeseballs etc on its menu. It also has drinks like lime soda costing ₹40/- and up. Fruitful also serves momos. The fries cost ₹50/- for the normal ones and toppings like cheese can be added for an extra 10 bucks while the burgers cost 90 bucks. It is generally open in the evenings till 9 PM.",
     svg: `${svg3}`,
     backcolor: " #E8A7E5",
+    id: "fruitful",
   },
   fourth: {
     name: "Maggi Hotspot",
@@ -35,6 +38,7 @@ export const fooddata = {
       "Open from 8 AM to 2 AM, it is the right place to try out all the variations in Maggi along with pasta and cold drinks. After a long, tough day, you can grab a quick snack from this outlet along with some ice cream from Amul, which is located right next to Maggi Hotspot. The hot chocolate will soothe your mind with all its richness, all with an average cost of around 35/-. Missed a meal at the mess? Fill your hungry tummy here with the infamous Vegetable Soup Maggi.",
     svg: `${svg4}`,
     backcolor: "#EC3B7F",
+    id: "maggi",
   },
   fivth: {
     name: "Euro Cafe",
@@ -42,6 +46,7 @@ export const fooddata = {
       "Euro cafe is the on-campus restaurant, a place where you'd go when you want to have a proper meal and have time to spare. Here you could get a full course meal from naan, several vegs and non-veg curries to biryani to tandoor items and Chinese items. It opens around 11 AM and closes at 10 PM. The chicken and paneer curries are priced around 120 bucks and upwards while the biryani costs 150 bucks.",
     svg: `${svg5}`,
     backcolor: " #ed8f72",
+    id: "euro",
   },
   sixth: {
     name: "Yummpy's",
@@ -49,6 +54,7 @@ export const fooddata = {
       "As the name suggests, this is your stop shop for a quick Bite. Known for its sandwiches this place offers snacks at an average affordable price all around ₹40. While you are on campus, it’s your safe haven for your late-night cravings. Yummpys is open from 9 AM to 2 AM There. You can get Maggi boiled or fried paneer egg or chicken at pocket-friendly rates Yummpys also serves omelettes and fried rice. The fries they serve late at night are always in high demand and are worth the hype and the money. They also serve noodles, chicken biryani, paneer nice; aloo paratha, onion paratha and several other dishes which are quite palatable and are worth trying from their menu. You would expect to spend around 30-50 bucks for Maggi while dishes like biryani and paneer rice would cost you 100 bucks. Yummpys also keeps a small stock of Havmor ice creams and serves coke/sprite of 20 bucks a glass, don't forget to try their famous pasta sandwich. Want to eat something different yet cheap and light? They do serve the all-loved classic Maggi which is also available in 2-3 variations, their timings being 5 PM-2 AM-on a regular day.",
     svg: `${svg6}`,
     backcolor: "#FFD60A",
+    id: "yummpy",
   },
   seventh: {
     name: "Nescafe",
@@ -56,6 +62,7 @@ export const fooddata = {
       "With college comes the evident love for coffee. Serving from 5 PM to 2 AM, this place is where you will find a myriad of club events taking place, with everyone either grooving to a musical event or laughing at the stand-ups. It is located right next to the VK lawns and it has a wonderful ambience with green lawns and warm, yellow lights. You'll find the best cheese oregano Maggi, where all dishes come at an average cost of 50 bucks. At Nescafe, you'll relish the taste of college life quite literally with perhaps some iced tea and a bar of KitKat.",
     svg: `${svg7}`,
     backcolor: "#EC3B7F",
+    id: "nescafe",
   },
   eight: {
     name: "SFC Burger Shop",
@@ -63,5 +70,6 @@ export const fooddata = {
       "A recent addition to the BPHC food scene, SFC has already carved its place in BITSians’ bellies. Located close to Mess 2, this food outlet opens from 2 PM to 2 AM every single day, and has an extremely diverse menu. Yearning for burgers and fries and can't wait until the weekend to crash at McDonald's? You can fancy your cravings at this store, with combos available at an average price of 100 bucks. Apart from the burgers and fries, this place also serves nuggets, pizzas along with regular special menus, SFC also stocks up on ice-creams from Baskin Robbins and offers homemade chocolates at their store regularly.",
     svg: `${svg8}`,
     backcolor: "#EC3B7F",
+    id: "sfc",
   },
 };
